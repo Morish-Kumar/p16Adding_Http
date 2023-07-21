e5056a8deed9a8a55d9168557209ad6514a1ffb3
singh-morish <morish.dtg@gmail.com>
2023-07-21 10:19:41 +0530

    adding base file

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './components/hooks/use-http';
+
+function App() {
+  const [tasks, setTasks] = useState([])
+
+  const transformTasks = taskObj => {
+    const loadedTasks = []
+    for(const taskKey in taskObj){
+      loadedTasks.push({id: taskKey, text: taskObj[taskKey].text})
+    }
+    setTasks(loadedTasks)
+  }
+
+  const httpData = useHttp({url: 'https://section16a-8034c-default-rtdb.firebaseio.com/tasks.json'}, transformTasks)
+
+
+  // transform task function will be called whenever we recive a response
+  
+  const {isLoading, error, sendRequest: fetchTasks} = httpData
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+
+    </React.Fragment>
+  );
+}
+
+export default App;
