392e8e504cddba7054c4cb51f751d4b1a049e533
singh-morish <morish.dtg@gmail.com>
2023-07-21 10:24:01 +0530

    adding lines in App js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
-import useHttp from './components/hooks/use-http';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./components/hooks/use-http";
 
 function App() {
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState([]);
 
-  const transformTasks = taskObj => {
-    const loadedTasks = []
-    for(const taskKey in taskObj){
-      loadedTasks.push({id: taskKey, text: taskObj[taskKey].text})
+  const transformTasks = (taskObj) => {
+    const loadedTasks = [];
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setTasks(loadedTasks)
-  }
+    setTasks(loadedTasks);
+  };
 
-  const httpData = useHttp({url: 'https://section16a-8034c-default-rtdb.firebaseio.com/tasks.json'}, transformTasks)
-
+  const httpData = useHttp(
+    { url: "https://section16a-8034c-default-rtdb.firebaseio.com/tasks.json" },
+    transformTasks
+  );
 
   // transform task function will be called whenever we recive a response
-  
-  const {isLoading, error, sendRequest: fetchTasks} = httpData
+
+  const { isLoading, error, sendRequest: fetchTasks } = httpData;
 
   useEffect(() => {
     fetchTasks();
@@ -32,14 +34,24 @@ function App() {
 
   return (
     <React.Fragment>
-      <NewTask onAddTask={taskAddHandler} />
-      <Tasks
-        items={tasks}
-        loading={isLoading}
-        error={error}
-        onFetch={fetchTasks}
-      />
-
+      <div>
+        <NewTask onAddTask={taskAddHandler} />
+        <Tasks
+          items={tasks}
+          loading={isLoading}
+          error={error}
+          onFetch={fetchTasks}
+        />
+      </div>
+      <div>
+        <NewTask onAddTask={taskAddHandler} />
+        <Tasks
+          items={tasks}
+          loading={isLoading}
+          error={error}
+          onFetch={fetchTasks}
+        />
+      </div>
     </React.Fragment>
   );
 }
